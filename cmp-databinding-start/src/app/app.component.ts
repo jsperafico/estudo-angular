d7defd1d24b4e6c44ac5bc36782b63b22ffe6fdd
jonathan-sperafico <jonathan-sperafico@PRO-PAE-0462>
2019-07-17 17:56:40 -0300

    component communication

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -8,19 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{ type: 'server', name: 'Test Server', content: 'Value' }];
 
-  onServerAdded() {
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onBlueprintAdded() {
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
   }
 }
